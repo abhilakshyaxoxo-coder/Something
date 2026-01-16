a39b31345d66b154063ae39d38d6904e9b6d7329
abhilakshyaxoxo-coder <abhilakshya577@gmail.com>
2026-01-16 12:08:09 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          900: '#0A192F',
+          800: '#112240',
+        },
+        gold: {
+          400: '#F6E05E',
+          500: '#D4AF37', // Classic Gold
+          600: '#B8860B',
+        },
+      },
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+}
